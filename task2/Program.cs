0b242d9aa571aa083a86a2f5948243c104a530d9
Олександр Паламарчук <a.palamar4uck@gmail.com>
2020-07-23 10:59:07 +0300

    Add project files.

---
+++ task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+                      System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int A;
+            int B;
+
+            bool f;
+            do
+            {
+                Console.WriteLine("Ввести A= ");
+                f = int.TryParse(Console.ReadLine(), out A);
+                if (!f)
+                {
+                    Console.WriteLine("Помилка введіть ще раз");
+                }
+            } while (!f);
+            do
+            {
+                Console.WriteLine("Ввести B= ");
+                f = int.TryParse(Console.ReadLine(), out B);
+                if (!f)
+                {
+                    Console.WriteLine("Помилка введіть ще раз");
+                }
+            } while (!f);
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i);
+                };
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
